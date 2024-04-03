2cb919151671fb90295db0ce60f33dfb68b63851
prashant-v-223 <prashantvadhvana@gmail.com>
2024-04-03 23:58:14 +0530

    qwe

--- src/component/Partnerstable.js
+++ src/component/Partnerstable.js
@@ -248,8 +248,8 @@ const PartnersTable = () => {
                 </tr>
               </thead>
               <tbody>
-                {houseRewards?.length > 0 ? (
-                  houseRewards
+                {levelRewards?.length > 0 ? (
+                  levelRewards
                     .sort((a, b) => new Date(b.Time) - new Date(a.Time)) // Sort by date in descending order
                     .filter((item) => item.level == 1) // Filter data where item.level is 1
                     .map((item, index) => {
@@ -327,20 +327,18 @@ const PartnersTable = () => {
                 </tr>
               </thead>
               <tbody>
-                {levelRewards?.length > 0 ? (
-                 levelRewards 
+                {houseRewards?.length > 0 ? (
+                  houseRewards
                     .sort((a, b) => new Date(b.Time) - new Date(a.Time)) // Sort by date in descending order
                     .map((item, index) => {
                       return (
                         <tr key={index}>
                           <td>
-                        
-                              <img
-                                src="/static/media/svg-image-23.aa0930be96db08ffc8e973487f0567fb.svg"
-                                alt="wallet"
-                                className="wallet_icon_last"
-                              />
-                            
+                            <img
+                              src="/static/media/svg-image-23.aa0930be96db08ffc8e973487f0567fb.svg"
+                              alt="wallet"
+                              className="wallet_icon_last"
+                            />
                           </td>
                           <td>{item.plan_name}</td>
                           <td>
